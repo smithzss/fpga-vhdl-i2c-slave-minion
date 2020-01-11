e62dba681c810e00d75aca80cf2e96edfbc04e14
Zach Smith <zach.smith@lightspeed-technologies.com>
2020-01-11 12:09:44 -0600

    add reset logic for default signal values

--- debounce.vhd
+++ debounce.vhd
@@ -19,13 +19,14 @@ entity debounce is
   port (
     signal_in  : in  std_logic;
     signal_out : out std_logic;
-    clk        : in  std_logic);
+    clk        : in  std_logic;
+    rst        : in  std_logic);
 end entity debounce;
 ------------------------------------------------------------
 architecture arch of debounce is
   type state_t is (idle, check_input_stable);
-  signal state_reg     : state_t                          := idle;
-  signal out_reg       : std_logic                        := signal_in;
+  signal state_reg     : state_t;--                          := idle;
+  signal out_reg       : std_logic;--                        := signal_in;
   signal signal_in_reg : std_logic;
   signal counter       : integer range 0 to WAIT_CYCLES-1 := 0;
 begin
@@ -33,27 +34,32 @@ begin
   process (clk) is
   begin
     if rising_edge(clk) then
-      case state_reg is
-        when idle =>
-          if out_reg /= signal_in then
-            signal_in_reg <= signal_in;
-            state_reg     <= check_input_stable;
-            counter       <= WAIT_CYCLES-1;
-          end if;
+      if (rst = '1') then
+        state_reg <= idle;
+        out_reg <= signal_in;
+      else
+          case state_reg is
+            when idle =>
+              if out_reg /= signal_in then
+                signal_in_reg <= signal_in;
+                state_reg     <= check_input_stable;
+                counter       <= WAIT_CYCLES-1;
+              end if;
 
-        when check_input_stable =>
-          if counter = 0 then
-            if signal_in = signal_in_reg then
-              out_reg <= signal_in;
-            end if;
-            state_reg <= idle;
-          else
-            if signal_in /= signal_in_reg then
-              state_reg <= idle;
-            end if;
-            counter <= counter - 1;
-          end if;
-      end case;
+            when check_input_stable =>
+              if counter = 0 then
+                if signal_in = signal_in_reg then
+                  out_reg <= signal_in;
+                end if;
+                state_reg <= idle;
+              else
+                if signal_in /= signal_in_reg then
+                  state_reg <= idle;
+                end if;
+                counter <= counter - 1;
+              end if;
+          end case;
+      end if;
     end if;
   end process;
 
